0b97eb794158f437ad7974f450b59f377e8af64e
MiniTigra <minitigrayt@gmail.com>
2025-07-21 23:45:29 -0400

    Updates

--- src/sections/TitleSection.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import TitleCard from "../lib/TitleCard.svelte";
-    
-    const title = "Balance the Block";
-    const subtitle = "A story of Overtown, Economics, and the Neighborhoods We Shape";
-</script>
-
-<TitleCard {title} {subtitle} />
